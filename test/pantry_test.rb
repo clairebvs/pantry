b3d71801ccc26326efb158c4578e00d6ed565467
ClaireBvs <clairembeauvais@gmail.com>
2018-06-05 09:08:16 -0600

    implements class Pantry and method stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_has_empty_stock_first
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_can_check_stock_is_empty
+    
+  end
+
+
+# pantry.stock_check("Cheese")
+# # => 0
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
+# ```
+
+
 end
